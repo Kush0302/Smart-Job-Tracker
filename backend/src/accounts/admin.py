14bb8a7fc91d0588e06d46b443e876e80844ce2a
Kush0302 <kush0302.patel@gmail.com>
2026-01-13 19:14:48 +0530

    Tested API Edge Cases, Permissions

--- backend/src/accounts/admin.py
+++ backend/src/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'username', 'is_staff', 'is_active')  #columns in admin list
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+# Register your custom user with this admin
+admin.site.register(User, UserAdmin)
+
